97e0b9e34bf3674eedf05a1734fb825884547ed3
Joe Dallago <jd.dallago@gmail.com>
2014-03-09 17:40:37 +0000

    Tabs -> spaces

--- demo/assets/demo.js
+++ demo/assets/demo.js
@@ -1,13 +1,13 @@
 var addEvent = function addEvent(element, eventName, func) {
-	if (element.addEventListener) {
-    	return element.addEventListener(eventName, func, false);
+    if (element.addEventListener) {
+        return element.addEventListener(eventName, func, false);
     } else if (element.attachEvent) {
         return element.attachEvent("on" + eventName, func);
     }
 };
 
 addEvent(document.getElementById('open-left'), 'click', function(){
-	snapper.open('left');
+    snapper.open('left');
 });
 
 /* Prevent Safari opening links when viewing as a Mobile App */
