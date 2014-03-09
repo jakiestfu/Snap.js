95620b0f568468f3cb6f41166e81ae4bcabaeabb
Joe Dallago <jd.dallago@gmail.com>
2014-03-09 23:46:41 +0000

    Created some starter tests to just see how Snap.js can be tested.

--- test/tests.js
+++ test/tests.js
@@ -57,3 +57,110 @@ describe("Translations", function() {
     });
 
 });
+
+describe("utils.eventType", function() {
+
+    it("It should return 'touchstart' when touch is supported.", function(done) {
+        Snap.utils.hasTouch = true;
+        var down = Snap.utils.eventType('down');
+        expect(down).to.equal('touchstart');
+
+        done();
+    });
+
+    it("It should return 'mousedown' when touch isn't supported and settings.clickToDrag is enabled.", function(done) {
+        Snap.utils.hasTouch = false;
+        var down = Snap.utils.eventType('down');
+        expect(down).to.equal('mousedown');
+
+        done();
+    });
+
+    it("It should return '' when touch isn't supported and settings.clickToDrag is disabled.", function(done) {
+        Snap.utils.hasTouch = false;
+        Snap.settings.clickToDrag = false;
+        var down = Snap.utils.eventType('down');
+        expect(down).to.equal('');
+
+        Snap.settings.clickToDrag = true;
+        done();
+    });
+
+    it("It should return 'touchmove' when touch is supported.", function(done) {
+        Snap.utils.hasTouch = true;
+        var move = Snap.utils.eventType('move');
+        expect(move).to.equal('touchmove');
+
+        done();
+    });
+
+    it("It should return 'mousemove' when touch isn't supported and settings.clickToDrag is enabled.", function(done) {
+        Snap.utils.hasTouch = false;
+        var move = Snap.utils.eventType('move');
+        expect(move).to.equal('mousemove');
+
+        done();
+    });
+
+    it("It should return '' when touch isn't supported and settings.clickToDrag is disabled.", function(done) {
+        Snap.utils.hasTouch = false;
+        Snap.settings.clickToDrag = false;
+        var move = Snap.utils.eventType('move');
+        expect(move).to.equal('');
+
+        Snap.settings.clickToDrag = true;
+        done();
+    });
+
+    it("It should return 'touchend' when touch is supported.", function(done) {
+        Snap.utils.hasTouch = true;
+        var up = Snap.utils.eventType('up');
+        expect(up).to.equal('touchend');
+
+        done();
+    });
+
+    it("It should return 'mouseup' when touch isn't supported and settings.clickToDrag is enabled.", function(done) {
+        Snap.utils.hasTouch = false;
+        var up = Snap.utils.eventType('up');
+        expect(up).to.equal('mouseup');
+
+        done();
+    });
+
+    it("It should return '' when touch isn't supported and settings.clickToDrag is disabled.", function(done) {
+        Snap.utils.hasTouch = false;
+        Snap.settings.clickToDrag = false;
+        var up = Snap.utils.eventType('up');
+        expect(up).to.equal('');
+
+        Snap.settings.clickToDrag = true;
+        done();
+    });
+
+    it("It should return 'touchcancel' when touch is supported.", function(done) {
+        Snap.utils.hasTouch = true;
+        var out = Snap.utils.eventType('out');
+        expect(out).to.equal('touchcancel');
+
+        done();
+    });
+
+    it("It should return 'mouseout' when touch isn't supported and settings.clickToDrag is enabled.", function(done) {
+        Snap.utils.hasTouch = false;
+        var out = Snap.utils.eventType('out');
+        expect(out).to.equal('mouseout');
+
+        done();
+    });
+
+    it("It should return '' when touch isn't supported and settings.clickToDrag is disabled.", function(done) {
+        Snap.utils.hasTouch = false;
+        Snap.settings.clickToDrag = false;
+        var out = Snap.utils.eventType('out');
+        expect(out).to.equal('');
+
+        Snap.settings.clickToDrag = true;
+        done();
+    });
+});
