32c739bae19478774056e1c93da66c5041b10270
Joe Dallago <jd.dallago@gmail.com>
2014-03-09 17:36:23 +0000

    Added test coverage with blanket.js.

--- test/tests.js
+++ test/tests.js
@@ -1,54 +1,59 @@
-var snapper = new Snap({
-	element: document.getElementById('content')
-});
+var _makeOne = function() {
+    return new Snap({
+        element: document.getElementById('content')
+    });
+};
 
 describe("Translations", function() {
 
-	it("It should open Left", function(done) {
+    it("It should open Left", function(done) {
+        var snapper = _makeOne();
 
-		snapper.on('animated', function(t){
-			var data = snapper.state();
+        snapper.on('animated', function(t){
+            var data = snapper.state();
 
-			expect( data.state ).to.equal( 'left' );
+            expect( data.state ).to.equal( 'left' );
 
-			snapper.off('animated');
-			done();
-		});
+            snapper.off('animated');
+            done();
+        });
 
-		snapper.open('left');
+        snapper.open('left');
 
-	});
-
-
-	it("It should open Right", function(done) {
-
-		snapper.on('animated', function(t){
-			var data = snapper.state();
-
-			expect( data.state ).to.equal( 'right' );
-
-			snapper.off('animated');
-			done();
-		});
-
-		snapper.open('right');
-
-	});
+    });
 
 
-	it("It should close", function(done) {
+    it("It should open Right", function(done) {
+        var snapper = _makeOne();
 
-		snapper.on('animated', function(t){
-			var data = snapper.state();
+        snapper.on('animated', function(t){
+            var data = snapper.state();
 
-			expect( data.state ).to.equal( 'closed' );
+            expect( data.state ).to.equal( 'right' );
 
-			snapper.off('animated');
-			done();
-		});
+            snapper.off('animated');
+            done();
+        });
 
-		snapper.close();
+        snapper.open('right');
 
-	});
+    });
+
+
+    it("It should close", function(done) {
+        var snapper = _makeOne();
+
+        snapper.on('animated', function(t){
+            var data = snapper.state();
+
+            expect( data.state ).to.equal( 'closed' );
+
+            snapper.off('animated');
+            done();
+        });
+
+        snapper.close();
+
+    });
 
 });
